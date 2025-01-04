1f8fa4d66551e1fcb817a3adb6371b01acaa5d74
rupeshkofficial <officialrupeshk@gmail.com>
2025-01-04 15:41:02 +0530

    up

--- Exception handling/p1.py
+++ Exception handling/p1.py
@@ -1,4 +1,10 @@
 try:
     a = 20 / 0
+    #a = 20 /20
+    print(a)
 except Exception as e:
     print("Error Occured in try block",e)
+else:
+    print("No error occured in try block, try block executed successfully!")
+finally:
+    print("Finally Block Executed Successfully!")
